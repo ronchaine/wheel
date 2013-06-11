eb60d5643f527c21bd2d13e060a39a1e1d7f4992
Jari Ronkainen <ronchaine@gmail.com>
2013-06-11 22:24:06 +0300

    Improved output

--- examples/stringhash.cpp
+++ examples/stringhash.cpp
@@ -13,22 +13,31 @@ int main(void)
 
    size_t hash;
 
+   int collision_count = 0;
+   int total_words = 0;
+   int duplicates = 0;
+
    while(getline(std::cin, input))
    {
-
+      total_words++;
       wheel::string output(input);
       hash = output.hash();
 
       if ((it = hashlist.find(hash)) != hashlist.end())
       {
          if (it->second == output)
-            std::cout << "Duplicate entry '" << output << "'\n";
+            duplicates++;
          else
+         {
             std::cout << "Hash collision with " << output << " and " << it->second << "::" << hash << "\n";
+            collision_count++;
+         }
       } else {
          hashlist.insert(std::pair<size_t, wheel::string>(hash, output));
       }
    }
 
-   return 0;
+   std::cout << "Words checked:" << total_words << " (" << total_words - duplicates << ")  Collisions:" << collision_count << "\n";
+
+   return collision_count;
 }
