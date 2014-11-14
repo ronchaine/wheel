ffa02afeadbfa85b2aad5d3ef69004bdde9ee065
Jari Ronkainen <ronchaine@gmail.com>
2014-11-14 12:27:51 +0200

    Added texturing functions.

--- include/wheel_module_video.h
+++ include/wheel_module_video.h
@@ -225,12 +225,18 @@ namespace wheel
 
             virtual  uint32_t DrawRect(int32_t x, int32_t y, uint32_t w, uint32_t h, float rot = 0.0f, int32_t x_pivot = 0, int32_t y_pivot = 0) = 0;
 
-            virtual  uint32_t AddTexture(const string& name, uint32_t w, uint32_t h, uint32_t fmt) { return WHEEL_UNIMPLEMENTED_FEATURE; }
-
             virtual  uint32_t Rotate(float x, float y, float z, float w) { return WHEEL_UNIMPLEMENTED_FEATURE; }            
 
             virtual  uint32_t Translate(float x, float y, float z) { return WHEEL_UNIMPLEMENTED_FEATURE; }
             virtual  uint32_t Translatei(int32_t x, int32_t y, int32_t z) { return WHEEL_UNIMPLEMENTED_FEATURE; }
+
+            // Atlasing / Texturing
+            virtual  void     CreateAtlas(const wcl::string& name, uint32_t size, uint32_t components);
+            virtual  void     CreateTexture(const wcl::string& name, uint32_t w, uint32_t h, uint32_t components, uint32_t format);
+            virtual  void     DeleteTexture(const wcl::string& name);
+            virtual  void     UpdateTextureData(const wcl::string& name, int32_t x_off, int32_t y_off, size_t w, size_t h, void* pixel_data);
+
+            virtual  void     SetTexture(uint32_t tu, const wcl::string& name);
       };
 
    }
